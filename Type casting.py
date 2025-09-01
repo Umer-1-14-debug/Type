9bcacf92ac887ccdfe419adab4134013352a8ab7
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-09-01 17:45:57 +0500

    Add files via upload

---
+++ Type casting.py
@@ -0,0 +1,6 @@
+name="99"
+print("Name is type of",type(name))
+new_name=float(name)
+print("New_name is type of",type(new_name))
+new_integer=int(new_name)
+print("New_integer is type of",type(new_integer))
